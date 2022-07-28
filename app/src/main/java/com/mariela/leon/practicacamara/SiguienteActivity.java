c230cd5090d496609f58beba7dd363815ed125e8
Lenovo <mariela.leon.est@tecazuay.edu.ec>
2022-07-27 20:04:04 -0600

    Subiendo

---
+++ app/src/main/java/com/mariela/leon/practicacamara/SiguienteActivity.java
@@ -0,0 +1,14 @@
+package com.mariela.leon.practicacamara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SiguienteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_siguiente);
+    }
+}
